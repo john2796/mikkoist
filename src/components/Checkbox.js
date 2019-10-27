e605a1407c8529e5a7b37899c9df723c06d8ec70
john2796 <jbmiranda22796@gmail.com>
2019-10-27 13:21:21 -0700

    setup component for adding task and adding project

--- src/components/Checkbox.js
+++ src/components/Checkbox.js
@@ -1,22 +1,34 @@
 import React from "react"
-import { firebase } from "firebase"
+import PropTypes from "prop-types"
+import { firebase } from "../firebase"
 
-export const Checkbox = ({ id }) => {
-  const archivedTask = () => {
+export const Checkbox = ({ id, taskDesc }) => {
+  const archiveTask = () => {
     firebase
       .firestore()
       .collection("tasks")
       .doc(id)
-      .update({ archived: true })
+      .update({
+        archived: true
+      })
   }
 
   return (
     <div
       className="checkbox-holder"
       data-testid="checkbox-action"
-      onClick={() => archivedTask()}
+      onClick={() => archiveTask()}
+      onKeyDown={() => archiveTask()}
+      aria-label={`Mark ${taskDesc} as done?`}
+      role="button"
+      tabIndex={0}
     >
-      <span className="checkbox"></span>
+      <span className="checkbox" />
     </div>
   )
+}
+
+Checkbox.propTypes = {
+  id: PropTypes.string.isRequired,
+  taskDesc: PropTypes.string.isRequired
 }
